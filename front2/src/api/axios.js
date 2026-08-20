270d18eb738aaa111ae7a48f0027ae5f90367863
jinjin <jinjinx28@gmail.com>
2026-08-20 17:09:11 +0900

    update api base url to render

--- front2/src/api/axios.js
+++ front2/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://smart-bicycle-service.onrender.com/api",
   timeout: 10000,
 });
 
